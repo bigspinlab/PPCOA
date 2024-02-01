3951b35c151b28ab0c4688311a9482fc337e9914
vitor-martinss <vitormartinsweb@gmail.com>
2024-02-01 21:55:28 +0000

    feat(FE): add form to contact page

---
+++ src/ppcoa-ssg-fe/src/components/FormContact/index.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { Button } from '@/components/Button';
+import { FormField, FormItem, FormLabel, FormControl, FormDescription, FormMessage, Form } from '@/components/Form';
+import RootWrapper from '@/components/RootWrapper';
+import { Input } from '@/components/Input';
+import { useForm } from 'react-hook-form'; 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { z } from "zod"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+export default function FormContact() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <RootWrapper>
+      <article>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </article>
+    </RootWrapper>
+  );
+}
