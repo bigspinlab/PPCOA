91b45af8cb79d1d1cb913bf7a0b5025b89e770c3
vitor-martinss <vitormartinsweb@gmail.com>
2024-02-06 18:41:46 +0000

    feat(FE): add max width

--- src/ppcoa-ssg-fe/src/components/RootWrapper/index.tsx
+++ src/ppcoa-ssg-fe/src/components/RootWrapper/index.tsx
@@ -6,5 +6,5 @@ interface RootWrapperProps {
 }
 
 export default function RootWrapper({ children, customClassName = '' }: RootWrapperProps) {
-  return <section className={`px-4 md:px-16 xl:px-32 ${customClassName}`}>{children}</section>;
+  return <section className={`px-4 md:px-16 xl:px-32 max-w-screen-2xl m-auto ${customClassName}`}>{children}</section>;
 }
