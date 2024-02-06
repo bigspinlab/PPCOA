f941d87972a35d497f02a0d493797357567be035
vitor-martinss <vitormartinsweb@gmail.com>
2024-02-06 21:39:46 +0000

    feat(FE): active link - wip

--- src/ppcoa-ssg-fe/src/components/NavRouteList/index.tsx
+++ src/ppcoa-ssg-fe/src/components/NavRouteList/index.tsx
@@ -1,26 +1,29 @@
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface NavRouteListProps {
   onRouteClick: () => void;
 }
 
 export default function NavRouteList({ onRouteClick }: NavRouteListProps) {
+  const isActive = usePathname().includes('about')
+  
   return (
-    <ul className="flex flex-col px-4 py-3 lg:flex-row gap-5 lg:p-0">
+    <ul className="flex flex-col px-4 py-3 lg:flex-row lg:items-center gap-5 lg:p-0">
       <li className="text-right text-4xl font-extralight lg:text-3xl">
-        <Link className="animate-in" href="/about" onClick={onRouteClick}>
+        <Link className={isActive?"font-bold":''} href="/about" onClick={onRouteClick}>
           Sobre
         </Link>
       </li>
 
       <li className="text-right text-4xl font-extralight lg:text-3xl">
-        <Link className="animate-in" href="/team" onClick={onRouteClick}>
+        <Link className={isActive?"font-bold":''} href="/team" onClick={onRouteClick}>
           Equipa
         </Link>
       </li>
 
       <li className="text-right text-4xl font-extralight lg:text-3xl">
-        <Link className="animate-in" href="/contact" onClick={onRouteClick}>
+        <Link className={isActive?"font-bold":''} href="/contact" onClick={onRouteClick}>
           Contactos
         </Link>
       </li>
