15d73b45100539e86bf408996f5e25ffdb75f4bb
vitor-martinss <vitormartinsweb@gmail.com>
2024-02-09 17:47:51 +0000

    feat(FE): add json server

---
+++ src/ppcoa-ssg-fe/src/lib/getHeadless.ts
@@ -0,0 +1,26 @@
+interface GetHeadlessProps {
+  route: string;
+}
+
+export const getHeadless = async ({ route }: GetHeadlessProps) => {
+  //const masterPageParam = isMasterPage ? 'handle=master$' : '';
+  //const mountedMarketCode = `https://localhost:8000/${languageCode}/${marketKey}/`;
+  //const url = `https://api.${environmentMap[env]}.bigspinlab.com/cx-cslight-headless-contentdelivery/handler?${masterPageParam}url=${mountedMarketCode}`;
+
+  const url = `http://localhost:8000/${route}`
+  const response = await fetch(url, {
+    method: 'GET',
+    // headers: {
+    //   'Content-Type': 'application/json',
+    //   'X-Api-Key': enterspeedApiKey
+    // }
+  });
+
+  // if (isMasterPage) {
+  //   const umbracoMasterPage = await response.json();
+  //   return umbracoMasterPage;
+  // }
+
+  const umbracoContent = await response.json();
+  return umbracoContent;
+};
