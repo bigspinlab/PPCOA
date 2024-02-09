62702759aa2707b649796241ddeadcb9cee05851
vitor-martinss <vitormartinsweb@gmail.com>
2024-02-09 21:35:47 +0000

    feat(FE): skeletons

--- src/ppcoa-ssg-fe/src/app/contact/loading.tsx
+++ src/ppcoa-ssg-fe/src/app/contact/loading.tsx
@@ -1,4 +1,21 @@
+import RootWrapper from '@/components/RootWrapper';
+import { Skeleton } from '@/ui-elements/Skeleton';
+
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <div className="fixed top-16 h-10 border-8 p-6 ">Loading contact</div>;
+  return (
+    <RootWrapper customClassName="w-full">
+      <div className="flex flex-col pt-44">
+        <Skeleton className="h-10 w-2/4 mb-9 sm:w-64 md:mb-20 lg:mb-28" />
+        <Skeleton className="w-full h-24 mb-24 sm:w-3/4" />
+        <div className="w-full mb-24 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-20">
+          <Skeleton className="h-64" />
+          <Skeleton className="h-64" />
+          <Skeleton className="h-64" />
+        </div>
+        <div className="flex flex-col items-start grow">
+          <Skeleton className="h-64 space-y-8 w-full md:w-4/5 lg:w-1/2"></Skeleton>
+        </div>
+      </div>
+    </RootWrapper>
+  );
 }
--- src/ppcoa-ssg-fe/src/app/privacy-policy/loading.tsx
+++ src/ppcoa-ssg-fe/src/app/privacy-policy/loading.tsx
@@ -1,4 +1,19 @@
+import RootWrapper from '@/components/RootWrapper';
+import { Skeleton } from '@/ui-elements/Skeleton';
+
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return <div className="fixed top-16 h-10 border-8 p-6 ">Loading Privacy policy</div>;
+  return (
+    <RootWrapper customClassName="w-full">
+      <div className="flex flex-col pt-44">
+        <Skeleton className="h-10 w-2/4 mb-9 sm:w-64 md:mb-20 lg:mb-28" />
+        <Skeleton className="w-full h-14 mb-24 sm:w-3/4" />
+        <div className="flex flex-col items-start">
+          <Skeleton className="h-64 space-y-8 w-full"></Skeleton>
+          <Skeleton className="h-64 space-y-8 w-full"></Skeleton>
+          <Skeleton className="h-64 space-y-8 w-full"></Skeleton>
+          <Skeleton className="h-64 space-y-8 w-full"></Skeleton>
+        </div>
+      </div>
+    </RootWrapper>
+  );
 }
