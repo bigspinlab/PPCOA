c5513710373a1f8986d74f967b97e3e1221c7d23
vitor-martinss <vitormartinsweb@gmail.com>
2024-02-11 14:08:15 +0000

    feat(FE): remove commented code

--- src/ppcoa-ssg-fe/src/components/ProjectsList/index.tsx
+++ src/ppcoa-ssg-fe/src/components/ProjectsList/index.tsx
@@ -28,36 +28,3 @@ export default async function ProjectsList() {
     </RootWrapper>
   );
 }
-
-// export const getStaticProps: GetStaticProps = async ({ params }) => {
-
-//   if (!params) {
-//     return {
-//       notFound: true
-//     };
-//   }
-
-//   try {
-//     const umbracoContent = await getHeadless();
-
-//     if (umbracoContent.meta?.Status === 404) {
-//       throw new Error('Error to fetch the Umbraco Content');
-//     }
-
-//     console.log('umbracoContent', umbracoContent);
-
-//     return {
-//       props: {
-//         umbracoContent
-//       },
-//       revalidate: 60
-//     };
-
-//   } catch (error) {
-//     console.error(error);
-//     return {
-//       notFound: true
-//     }
-//   }
-
-// }
