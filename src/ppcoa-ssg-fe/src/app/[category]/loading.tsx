a6e33a322c87d035fe07277c6da4d6fc4e8cb217
vitor-martinss <vitormartinsweb@gmail.com>
2024-02-11 14:15:35 +0000

    feat(FE): improve skeleton project list

--- src/ppcoa-ssg-fe/src/app/[category]/loading.tsx
+++ src/ppcoa-ssg-fe/src/app/[category]/loading.tsx
@@ -4,9 +4,36 @@ import { Skeleton } from '@/ui-elements/Skeleton';
 export default function Loading() {
   return (
     <RootWrapper customClassName="w-full">
-      <div className="flex flex-col pt-44">
-        <Skeleton className="h-10 w-2/4 mb-9 sm:w-64 md:mb-20 lg:mb-28" />
-        <Skeleton className="w-full h-24 mb-24 sm:w-3/4" />
+      <div className="flex flex-col pt-14 md:pt-44">
+        <ul className="w-full max-w-[550px] grid grid-rows-1 m-auto gap-16 lg:gap-20">
+          <li>
+            <div className="flex flex-col gap-1.5">
+              <Skeleton className="h-[550px] w-[550px]" />
+              <div className="flex flex-col gap-2">
+                <Skeleton className="h-7 w-full" />
+                <Skeleton className="h-6 w-full " />
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="flex flex-col gap-1.5">
+              <Skeleton className="h-[550px] w-[550px]" />
+              <div className="flex flex-col gap-2">
+                <Skeleton className="h-7 w-full" />
+                <Skeleton className="h-6 w-full " />
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className="flex flex-col gap-1.5">
+              <Skeleton className="h-[550px] w-[550px]" />
+              <div className="flex flex-col gap-2">
+                <Skeleton className="h-7 w-full" />
+                <Skeleton className="h-6 w-full " />
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
     </RootWrapper>
   );
