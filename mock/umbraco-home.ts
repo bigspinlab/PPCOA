f635eea5a32be6541599f32014b2ebeaaa4152f1
vitor-martinss <vitormartinsweb@gmail.com>
2024-02-13 22:31:11 +0000

    feat(FE): mock

--- mock/umbraco-home.ts
+++ mock/umbraco-home.ts
@@ -1,97 +1,106 @@
 export const projectsListMock = {
-    id: "projectsList",
-    alias: "projectsList",
-    content: [
-      {
-        id: 1,
-        title: "Project 1",
-        category: "archite",
-        imageSrc: {
-          desktop: "https://images.unsplash.com/photo-1488972685288-c3fd157d7c7a?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHwxfHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3",
-          alt: "Project 1"
-        }
-      },
-      {
-        id: 2,
-        title: "Project 1",
-        category:" urban,",
-        imageSrc: {
-          desktop: "https://images.unsplash.com/photo-1487958449943-2429e8be8625?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHwyfHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3",
-          alt: "Project 1"
-        }
-      },
-      {
-        id: 3,
-        title: "Project 1",
-        category: "society",
-        imageSrc: {
-          desktop: "https://images.unsplash.com/photo-1435575653489-b0873ec954e2?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw0fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3",
-          alt: "Project 1"
-        }
-      },
-      {
-        id: 4,
-        title: "Project 1",
-        category: "society",
-        imageSrc: {
-          desktop: "https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw1fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3",
-          alt: "Project 1"
-        }
-      },
-      {
-        id: 5,
-        title: "Project 1",
-        category: "society",
-        imageSrc: {
-          desktop: "https://images.unsplash.com/photo-1479839672679-a46483c0e7c8?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw2fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3",
-          alt: "Project 1"
-        }
-      },
-      {
-        id: 6,
-        title: "Project 1",
-        category: "society",
-        imageSrc: {
-          desktop: "https://images.unsplash.com/photo-1483366774565-c783b9f70e2c?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw3fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3",
-          alt: "Project 1"
-        }
-      },
-      {
-        id: 7,
-        title: "Project 1",
-        category: "society",
-        imageSrc: {
-          desktop: "https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw4fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3",
-          alt: "Project 1"
-        }
-      },
-      {
-        id: 8,
-        title: "Project 1",
-        category: "society",
-        imageSrc: {
-          desktop: "https://images.unsplash.com/photo-1492321936769-b49830bc1d1e?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw5fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3",
-          alt: "Project 1"
-        }
-      },
-      {
-        id: 9,
-        title: "Project 1",
-        category: "society",
-        imageSrc: {
-          desktop: "https://images.unsplash.com/photo-1431576901776-e539bd916ba2?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHwxMHx8YXJjaGl0ZWN0dXJlfGVufDB8fHx8MTcwNzY1MTE2N3ww&ixlib=rb-4.0.3",
-          alt: "Project 1"
-        }
-      },
-      {
-        id: 10,
-        title: "Project 1",
-        category: "society",
-        imageSrc: {
-          desktop: "https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw4fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3",
-          alt:"dsdsa"
-        }
+  id: 'projectsList',
+  alias: 'projectsList',
+  content: [
+    {
+      id: 1,
+      title: 'Project 1',
+      category: 'archite',
+      imageSrc: {
+        desktop:
+          'https://images.unsplash.com/photo-1488972685288-c3fd157d7c7a?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHwxfHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3',
+        alt: 'Project 1'
       }
-    ]
-  }
-
+    },
+    {
+      id: 2,
+      title: 'Project 1',
+      category: ' urban,',
+      imageSrc: {
+        desktop:
+          'https://images.unsplash.com/photo-1487958449943-2429e8be8625?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHwyfHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3',
+        alt: 'Project 1'
+      }
+    },
+    {
+      id: 3,
+      title: 'Project 1',
+      category: 'society',
+      imageSrc: {
+        desktop:
+          'https://images.unsplash.com/photo-1435575653489-b0873ec954e2?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw0fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3',
+        alt: 'Project 1'
+      }
+    },
+    {
+      id: 4,
+      title: 'Project 1',
+      category: 'society',
+      imageSrc: {
+        desktop:
+          'https://images.unsplash.com/photo-1511818966892-d7d671e672a2?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw1fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3',
+        alt: 'Project 1'
+      }
+    },
+    {
+      id: 5,
+      title: 'Project 1',
+      category: 'society',
+      imageSrc: {
+        desktop:
+          'https://images.unsplash.com/photo-1479839672679-a46483c0e7c8?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw2fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3',
+        alt: 'Project 1'
+      }
+    },
+    {
+      id: 6,
+      title: 'Project 1',
+      category: 'society',
+      imageSrc: {
+        desktop:
+          'https://images.unsplash.com/photo-1483366774565-c783b9f70e2c?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw3fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3',
+        alt: 'Project 1'
+      }
+    },
+    {
+      id: 7,
+      title: 'Project 1',
+      category: 'society',
+      imageSrc: {
+        desktop:
+          'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw4fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3',
+        alt: 'Project 1'
+      }
+    },
+    {
+      id: 8,
+      title: 'Project 1',
+      category: 'society',
+      imageSrc: {
+        desktop:
+          'https://images.unsplash.com/photo-1492321936769-b49830bc1d1e?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw5fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3',
+        alt: 'Project 1'
+      }
+    },
+    {
+      id: 9,
+      title: 'Project 1',
+      category: 'society',
+      imageSrc: {
+        desktop:
+          'https://images.unsplash.com/photo-1431576901776-e539bd916ba2?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHwxMHx8YXJjaGl0ZWN0dXJlfGVufDB8fHx8MTcwNzY1MTE2N3ww&ixlib=rb-4.0.3',
+        alt: 'Project 1'
+      }
+    },
+    {
+      id: 10,
+      title: 'Project 1',
+      category: 'society',
+      imageSrc: {
+        desktop:
+          'https://images.unsplash.com/photo-1451976426598-a7593bd6d0b2?ixid=M3w1NjU5MTZ8MHwxfHNlYXJjaHw4fHxhcmNoaXRlY3R1cmV8ZW58MHx8fHwxNzA3NjUxMTY3fDA&ixlib=rb-4.0.3',
+        alt: 'dsdsa'
+      }
+    }
+  ]
+};
