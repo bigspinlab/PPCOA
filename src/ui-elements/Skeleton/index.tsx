89c8aa9512db2bad2f366193dc9b1ee67c83b97e
vitor-martinss <vitormartinsweb@gmail.com>
2024-02-13 22:31:40 +0000

    feat(FE): form - wip

--- src/ui-elements/Skeleton/index.tsx
+++ src/ui-elements/Skeleton/index.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/lib/utils';
 import React from 'react';
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn('animate-pulse rounded-md bg-muted', className)} {...props} />;
+  return <div className={cn('animate-pulse  bg-muted', className)} {...props} />;
 }
 
 export { Skeleton };
