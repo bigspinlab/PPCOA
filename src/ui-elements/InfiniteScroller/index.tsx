c1d8df8e63503897db52d852af8d699d18c92ef1
vitor-martinss <vitormartinsweb@gmail.com>
2024-03-24 20:32:21 +0000

    feat(FE): create infinite scroll component - wip

---
+++ src/ui-elements/InfiniteScroller/index.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+interface InfiniteScrollProps extends React.HTMLAttributes<HTMLDivElement> {
+  fetchNextPage: () => void;
+  hasNextPage: boolean;
+  loadingMessage: React.ReactNode;
+  endingMessage: React.ReactNode;
+}
+
+// eslint-disable-next-line react/display-name
+export const InfiniteScroller = React.forwardRef<
+  HTMLDivElement,
+  InfiniteScrollProps
+>(
+  (
+    {
+      fetchNextPage,
+      hasNextPage,
+      endingMessage,
+      loadingMessage,
+      children,
+      ...props
+    },
+    ref
+  ) => {
+    const observerTarget = React.useRef(null);
+
+    React.useEffect(() => {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          if (entries[0]?.isIntersecting) fetchNextPage();
+        },
+        { threshold: 1 }
+      );
+
+      if (observerTarget.current) {
+        observer.observe(observerTarget.current);
+      }
+
+      return () => observer.disconnect();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+      <div ref={ref} {...props} style={{ overflowAnchor: "none" }}>
+        {children}
+        <div ref={observerTarget} />
+        {hasNextPage ? loadingMessage : endingMessage}
+      </div>
+    );
+  }
+);
