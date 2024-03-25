15b5fc4af0221fdc74e1078437eadc922852a383
vitor-martinss <vitormartinsweb@gmail.com>
2024-03-25 18:39:31 +0000

    feat(FE): umbraco integration content on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,52 +1,27 @@
+'use serve';
+
+import ColumnsContent from '@/components/ColumnsContent';
 import FormContact from '@/components/FormContact';
 import RootWrapper from '@/components/RootWrapper';
+import { getHeadless } from '@/lib/getHeadless';
 import Rectangle from '@/ui-elements/Rectangle';
+import { QueryClient } from '@tanstack/react-query';
 
-export default function Contact() {
+export default async function Contact() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['aboutContent'],
+    queryFn: () => getHeadless({ route: 'contact' })
+  });
+
   return (
     <>
       <RootWrapper>
         <h2 className="sr-only">Contact</h2>
         <article className="pt-44">
           <Rectangle customStyles="bg-red-100 w-2/4 mb-9 sm:w-64 md:mb-20 lg:mb-28" />
-          <div className="w-full mb-24 font-bold sm:w-3/4">
-            <h3 className="text-3xl">
-              Pretende falar connosco? Conforme a sua questão, encontre o contacto mais apropriado, ou envie-nos uma
-              mensagem. Adorávamos ouvir o que tem para nos dizer.
-            </h3>
-          </div>
-          <div className="w-full mb-24 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-20">
-            <div className="flex flex-col gap-5">
-              <h2>Sede</h2>
-              <p>Rua kwame Krumah, 256 Luanda, Angola</p>
-              <p>[PT] +351 962 452 491 [AO] +244 934 460 434</p>
-              <p>VAT 123456789</p>
-              <p>
-                <strong>Questões Gerais</strong>
-              </p>
-              <p>geral@pedropintocorreia.com</p>
-            </div>
-
-            <div className="flex flex-col gap-5">
-              <h2>Carreiras</h2>
-              <p>Pretende enviar o seu curriculo ou portfólio?</p>
-              <p>Envie email para carreiras@pedropintocorreia.com</p>
-              <p>Também pode enviar-nos uma mensagem preenchendo ou campos em baixo.</p>
-            </div>
-
-            <div className="flex flex-col gap-5">
-              <h2>Redes Sociais</h2>
-              <p>Siga-nos e fique a par os últimos projetos e novidades:</p>
-              <div className="flex gap-6 md:flex-col md:gap-0">
-                <a target="_blank" href="">
-                  Linkedin
-                </a>
-                <a target="_blank" href="">
-                  Instagram
-                </a>
-              </div>
-            </div>
-          </div>
+          <ColumnsContent />
         </article>
       </RootWrapper>
       <FormContact />
---
+++ src/components/ColumnsContent/index.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useGetHeadlessContent } from '@/hooks/useGetHeadlessContent';
+import parse from 'html-react-parser';
+
+export default function ColumnsContent() {
+  const { data: columnsContent } = useGetHeadlessContent({ route: 'contact', queryKey: 'contactContent' });
+
+  if (!columnsContent) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="w-full mb-24 font-bold sm:w-3/4">
+        <h3 className="text-3xl">{columnsContent.widgets[0].content.headline}</h3>
+      </div>
+      <div className="w-full mb-24 ">
+        <ul className="w-full grid grid-cols-1 gap-y-28 gap-x-10 sm:grid-cols-2 lg:grid-cols-3 lg:gap-20">
+          {columnsContent.widgets[0].content.columns.items.map((column: any) => (
+            <li key={column.id}>
+              <h2>{column.title}</h2>
+              <div>{parse(`${column.text}`)}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
