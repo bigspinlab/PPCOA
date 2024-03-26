d6583f6e39bf93a9131cea461c73b52034b6e463
vitor-martinss <vitormartinsweb@gmail.com>
2024-03-26 15:24:54 +0000

    fix(FE): fixed width on carousel text container

--- src/ui-elements/CarouselItem/index.tsx
+++ src/ui-elements/CarouselItem/index.tsx
@@ -43,7 +43,7 @@ const CarouselItemContent = ({ carouselItemType, content, settings, alias, id, c
       </div>
       {showContainerText ? (
         <div
-          className={`whitespace-normal py-5 px-4  md:py-7 md:px-9 ${tailwindClassName} ${carouselItemType === CAROUSEL_ITEM_TYPE.IMAGE_WITH_TEXT_SMALL_CONTAINER ? 'max-w-[166px] md:max-w-[300px]' : 'max-w-72 md:max-w-[400px]'}`}
+          className={`whitespace-normal py-5 px-4  md:py-7 md:px-9 ${tailwindClassName} ${carouselItemType === CAROUSEL_ITEM_TYPE.IMAGE_WITH_TEXT_SMALL_CONTAINER ? 'w-[166px] md:w-[300px]' : 'w-72 md:w-[400px]'}`}
         >
           {parse(`${content?.text}`)}
         </div>
