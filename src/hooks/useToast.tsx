3c6a8abf381372df5d9d0b9d20dedd880b587a34
vitor-martinss <vitormartinsweb@gmail.com>
2024-03-26 21:54:08 +0000

    feat(FE): toast delay removal - form

--- src/hooks/useToast.tsx
+++ src/hooks/useToast.tsx
@@ -6,7 +6,7 @@ import { ToastProps } from '@radix-ui/react-toast';
 import * as React from 'react';
 
 const TOAST_LIMIT = 1;
-const TOAST_REMOVE_DELAY = 1000000;
+const TOAST_REMOVE_DELAY = 4000;
 
 type ToasterToast = ToastProps & {
   id: string;
