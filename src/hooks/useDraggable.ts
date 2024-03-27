0ee15e5b7af4f72495c11c422965bf44cfc834e9
vitor-martinss <vitormartinsweb@gmail.com>
2024-03-27 12:45:22 +0000

    feat(FE): use draggable only on desktop

--- src/hooks/useDraggable.ts
+++ src/hooks/useDraggable.ts
@@ -1,68 +1,78 @@
 import React from 'react';
+import { useScreenSize } from './useScreenSize';
 
 export const useDraggable = (containerRef: React.RefObject<HTMLDivElement>) => {
+  const { isLargeAndUp } = useScreenSize();
 
-  const handleMouseDown = React.useCallback((e: React.MouseEvent) => {
-    const ele = containerRef.current;
-    if (!ele) {
-      return;
-    }
-    const startPos = {
-      left: ele.scrollLeft,
-      top: ele.scrollTop,
-      x: e.clientX,
-      y: e.clientY
-    };
+  const handleMouseDown = React.useCallback(
+    (e: React.MouseEvent) => {
 
-    const handleMouseMove = (e: MouseEvent) => {
-      const dx = e.clientX - startPos.x;
-      const dy = e.clientY - startPos.y;
-      ele.scrollTop = startPos.top - dy;
-      ele.scrollLeft = startPos.left - dx;
-      updateCursor(ele);
-    };
+      const ele = containerRef.current;
+      if (!ele) {
+        return;
+      }
+      const startPos = {
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        x: e.clientX,
+        y: e.clientY
+      };
 
-    const handleMouseUp = () => {
-      document.removeEventListener('mousemove', handleMouseMove);
-      document.removeEventListener('mouseup', handleMouseUp);
-      resetCursor(ele);
-    };
+      const handleMouseMove = (e: MouseEvent) => {
+        const dx = e.clientX - startPos.x;
+        const dy = e.clientY - startPos.y;
+        ele.scrollTop = startPos.top - dy;
+        ele.scrollLeft = startPos.left - dx;
+        updateCursor(ele);
+      };
 
-    document.addEventListener('mousemove', handleMouseMove);
-    document.addEventListener('mouseup', handleMouseUp);
-  }, [containerRef]);
+      const handleMouseUp = () => {
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+        resetCursor(ele);
+      };
 
-  const handleTouchStart = React.useCallback((e: React.TouchEvent) => {
-    const ele = containerRef.current;
-    if (!ele) {
-      return;
-    }
-    const touch = e.touches[0];
-    const startPos = {
-      left: ele.scrollLeft,
-      top: ele.scrollTop,
-      x: touch.clientX,
-      y: touch.clientY
-    };
+      document.addEventListener('mousemove', handleMouseMove);
+      document.addEventListener('mouseup', handleMouseUp);
+    },
+    [containerRef]
+  );
 
-    const handleTouchMove = (e: TouchEvent) => {
+  const handleTouchStart = React.useCallback(
+    (e: React.TouchEvent) => {
+
+      const ele = containerRef.current;
+      if (!ele) {
+        return;
+      }
       const touch = e.touches[0];
-      const dx = touch.clientX - startPos.x;
-      const dy = touch.clientY - startPos.y;
-      ele.scrollTop = startPos.top - dy;
-      ele.scrollLeft = startPos.left - dx;
-      updateCursor(ele);
-    };
+      const startPos = {
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        x: touch.clientX,
+        y: touch.clientY
+      };
 
-    const handleTouchEnd = () => {
-      document.removeEventListener('touchmove', handleTouchMove);
-      document.removeEventListener('touchend', handleTouchEnd);
-      resetCursor(ele);
-    };
+      const handleTouchMove = (e: TouchEvent) => {
+        const touch = e.touches[0];
+        const dx = touch.clientX - startPos.x;
+        const dy = touch.clientY - startPos.y;
+        ele.scrollTop = startPos.top - dy;
+        ele.scrollLeft = startPos.left - dx;
+        updateCursor(ele);
+      };
 
-    document.addEventListener('touchmove', handleTouchMove);
-    document.addEventListener('touchend', handleTouchEnd);
-  }, [containerRef]);
+      const handleTouchEnd = () => {
+        document.removeEventListener('touchmove', handleTouchMove);
+        document.removeEventListener('touchend', handleTouchEnd);
+        resetCursor(ele);
+      };
+
+      document.addEventListener('touchmove', handleTouchMove);
+      document.addEventListener('touchend', handleTouchEnd);
+    },
+    [containerRef]
+  );
 
   const updateCursor = (ele: HTMLDivElement) => {
     ele.style.cursor = 'grabbing';
@@ -73,6 +83,13 @@ export const useDraggable = (containerRef: React.RefObject<HTMLDivElement>) => {
     ele.style.cursor = 'grab';
     ele.style.removeProperty('user-select');
   };
+
+  if (!isLargeAndUp) {
+    return {
+      handleMouseDown: undefined,
+      handleTouchStart: undefined
+    };
+  }
 
   return {
     handleMouseDown,
