3b94cbc1997dde2b8e75857371d812044f9809c2
vitor-martinss <vitormartinsweb@gmail.com>
2024-03-27 22:03:31 +0000

    feat(FE): add generic type to getHeadless function

--- src/lib/getHeadless.ts
+++ src/lib/getHeadless.ts
@@ -2,7 +2,8 @@ interface GetHeadlessProps {
   route: string;
 }
 
-export const getHeadless = async ({ route }: GetHeadlessProps) => {
+// Define a type parameter T for the response data
+export const getHeadless = async <T>({ route }: GetHeadlessProps): Promise<T> => {
   const url = `https://danielribamar-001-site1.itempurl.com/api/v1/pages/${route}`;
   const response = await fetch(url, {
     method: 'GET',
@@ -11,6 +12,6 @@ export const getHeadless = async ({ route }: GetHeadlessProps) => {
     }
   });
 
-  const umbracoContent = await response.json();
-  return umbracoContent;
+  const responseData: T = await response.json();
+  return responseData;
 };
