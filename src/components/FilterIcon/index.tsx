369474d06f293f74b7f28411194c75ec2cda7aea
vitor-martinss <vitormartinsweb@gmail.com>
2024-03-28 20:02:31 +0000

    feat(FE): test urban icon alone

--- src/components/FilterIcon/index.tsx
+++ src/components/FilterIcon/index.tsx
@@ -1,6 +1,6 @@
 import { FilterIconAll } from '@/ui-elements/FilterIcon/FilterIconAll';
-import { FilterIconArchitecture } from '@/ui-elements/FilterIcon/FilterIconArchitecture';
-import { FilterIconContest } from '@/ui-elements/FilterIcon/FilterIconContest';
+// import { FilterIconArchitecture } from '@/ui-elements/FilterIcon/FilterIconArchitecture';
+// import { FilterIconContest } from '@/ui-elements/FilterIcon/FilterIconContest';
 import { FilterIconInteriors } from '@/ui-elements/FilterIcon/FilterIconInteriors';
 import { FilterIconUrban } from '@/ui-elements/FilterIcon/FilterIconUrban';
 import { ReactNode } from 'react';
@@ -13,8 +13,8 @@ interface FilterIconProps extends PickIHeaderNavigation {
 const FilterIcon = ({ id, themeColor, isActive }: FilterIconProps) => {
   const filterIcon: { [key: string]: ReactNode } = {
     [1066]: <FilterIconAll fillColor={`${isActive ? themeColor : '#fff'}`} />,
-    [1067]: <FilterIconArchitecture fillColor={`${isActive ? themeColor : '#fff'}`} />,
-    [1070]: <FilterIconContest fillColor={`${isActive ? themeColor : '#fff'}`} />,
+    // [1067]: <FilterIconArchitecture fillColor={`${isActive ? themeColor : '#fff'}`} />,
+    // [1070]: <FilterIconContest fillColor={`${isActive ? themeColor : '#fff'}`} />,
     [1068]: <FilterIconInteriors fillColor={`${isActive ? themeColor : '#fff'}`} />,
     [1069]: <FilterIconUrban fillColor={`${isActive ? themeColor : '#fff'}`} />
   };
