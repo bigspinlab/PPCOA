a356a863f458c7ab371bbe840f3d3889b4400ecc
vitor-martinss <vitormartinsweb@gmail.com>
2024-03-28 17:20:41 +0000

    feat(FE): revert from switch on filterItem

--- src/components/FilterItem/index.tsx
+++ src/components/FilterItem/index.tsx
@@ -6,6 +6,7 @@ import { FilterIconContest } from '@/ui-elements/FilterIcon/FilterIconContest';
 import { FilterIconInteriors } from '@/ui-elements/FilterIcon/FilterIconInteriors';
 import { FilterIconUrban } from '@/ui-elements/FilterIcon/FilterIconUrban';
 import Link from 'next/link';
+import { ReactNode } from 'react';
 
 interface FilterItemProps extends IHeaderNavigationCategories {
   isActive: boolean;
@@ -14,22 +15,13 @@ interface FilterItemProps extends IHeaderNavigationCategories {
 const FilterItem = ({ themeColor, id, label, url = '/', isActive }: FilterItemProps) => {
   const labelWithFirstLetterCapitalized = capitalizeFirstLetter(label);
 
-  const renderFilterIconSwitch = ({themeColor}: {themeColor: string}) => {
-    switch (themeColor) {
-      case '#ffffff':
-        return <FilterIconAll fillColor={`${isActive ? themeColor : '#fff'}`} />;
-      case '#fabd5c':
-        return <FilterIconArchitecture fillColor={`${isActive ? themeColor : '#fff'}`} />;
-      case '#6c757d':
-        return <FilterIconContest fillColor={`${isActive ? themeColor : '#fff'}`} />;
-      case '#006aad':
-        return <FilterIconInteriors fillColor={`${isActive ? themeColor : '#fff'}`} />;
-      case '#fa4647':
-        return <FilterIconUrban fillColor={`${isActive ? themeColor : '#fff'}`} />;
-      default:
-         return null
-    }
-  }
+  const filterIcon: { [key: string]: ReactNode } = {
+    ['#ffffff']: <FilterIconAll fillColor={`${isActive ? themeColor : '#fff'}`} />,
+    ['#fabd5c']: <FilterIconArchitecture fillColor={`${isActive ? themeColor : '#fff'}`} />,
+    ['#6c757d']: <FilterIconContest fillColor={`${isActive ? themeColor : '#fff'}`} />,
+    ['#006aad']: <FilterIconInteriors fillColor={`${isActive ? themeColor : '#fff'}`} />,
+    ['#fa4647']: <FilterIconUrban fillColor={`${isActive ? themeColor : '#fff'}`} />
+  };
 
   return (
     <Link
@@ -37,8 +29,8 @@ const FilterItem = ({ themeColor, id, label, url = '/', isActive }: FilterItemPr
       data-testid={`filter-item-${id}`}
       className="w-auto h-auto flex flex-col justify-center items-center shrink-0 p-0 px-3 md:px-4"
     >
-
-      {renderFilterIconSwitch({themeColor})}
+      
+      {filterIcon[themeColor]}
       <p className={`${isActive ? 'font-bold' : null} whitespace-nowrap`}>{labelWithFirstLetterCapitalized}</p>
     </Link>
   );
