05b25e96faea3c91145dc3f0e5361a8afcca9d5f
vitor-martinss <vitormartinsweb@gmail.com>
2024-03-28 16:31:52 +0000

    feat(FE): hydration of filters on layout

--- src/app/[category]/layout.tsx
+++ src/app/[category]/layout.tsx
@@ -1,10 +1,22 @@
 import { Filter } from '@/components/Filter';
+import { getHeadlessMaster } from '@/lib/getHeadlessMaster';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
 import React from 'react';
 
-export default function CategoriesLayout({ children }: { children: React.ReactNode }) {
+export default async function CategoriesLayout({ children }: { children: React.ReactNode }) {
+  
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['masterPage'],
+    queryFn: getHeadlessMaster
+  });
+
   return (
     <>
-      <Filter />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Filter />
+      </HydrationBoundary>
       {children}
     </>
   );
