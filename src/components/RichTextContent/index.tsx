4d1aef227b84d33c7e0f0d561fb48cb8c7a33bd2
vitor-martinss <vitormartinsweb@gmail.com>
2024-04-04 22:35:18 +0100

    feat(FE): improve typescript on privacy policy page components

--- src/components/RichTextContent/index.tsx
+++ src/components/RichTextContent/index.tsx
@@ -1,10 +1,14 @@
 'use client';
 
-import { useGetHeadlessContent } from '@/hooks/useGetHeadlessContent';
+import { getHeadless } from '@/api';
+import { ROUTES } from '@/global/constants';
+import { IHeadlessContentPage, IRichTextContent } from '@/types';
+import { useQuery } from '@tanstack/react-query';
 import parse from 'html-react-parser';
 
-export default function RichTextContent() {
-  const { data: richText } = useGetHeadlessContent({ route: 'privacy-policy', queryKey: 'privacyPolicyContent' });
+export default function RichTextContent({ params }: { params: { lang: string } }) {
+  const { data: richTextData } = useQuery<IHeadlessContentPage>({ queryKey: [ROUTES.privacyPolicy.queryKey], queryFn: () => getHeadless({ route: ROUTES.privacyPolicy.path, lang: params.lang }) });
+  const richText = richTextData?.widgets[0] as IRichTextContent
 
   if (!richText) {
     return null;
@@ -13,9 +17,9 @@ export default function RichTextContent() {
   return (
     <>
       <div className="w-full mb-24 font-bold sm:w-3/4">
-        <h3 className="text-3xl">{richText.widgets[0].content.title}</h3>
+        <h3 className="text-3xl">{richText?.content?.title}</h3>
       </div>
-      {parse(`${richText.widgets[0].content.richText}`)}
+      {parse(`${richText?.content?.richText}`)}
     </>
   );
 }
