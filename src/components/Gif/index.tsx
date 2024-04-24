e7787551d90508cbc2ca9332dab1ed7aab595720
vitor-martinss <vitormartinsweb@gmail.com>
2024-04-24 23:58:07 +0100

    feat: timeout increased for smooth animation

--- src/components/Gif/index.tsx
+++ src/components/Gif/index.tsx
@@ -12,7 +12,7 @@ function Gif({setRemoveAnimationBg}: {setRemoveAnimationBg: (value: React.SetSta
     if (!hasAnimationShown) {
       const timeout = setTimeout(() => {
         setHasAnimationShown(true);
-      }, 6550);
+      }, 6590);
 
       return () => clearTimeout(timeout);
     } else {
