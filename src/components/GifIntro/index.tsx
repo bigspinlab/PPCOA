ba6933bf448e5904c23c645ca8a060b965d77180
vitor-martinss <vitormartinsweb@gmail.com>
2024-04-24 17:13:55 +0100

    feat: add animation on storage and show it once

--- src/components/GifIntro/index.tsx
+++ src/components/GifIntro/index.tsx
@@ -1,21 +1,34 @@
 'use client'
 
+import useStorage from '@/hooks/useStorage';
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function GifIntro() {
-  const [showAnimation, setShowAnimation] = useState(true); 
+  const { getItem, setItem } = useStorage();
+  const [hasAnimationShown, setHasAnimationShown] = useState(false);
 
-  if (!showAnimation) {
+  useEffect(() => {
+    const token = getItem('ppcoaAnimation', 'local');
+    const hasShown = token === 'true';
+
+    if (!hasShown) {
+      // Start the animation after a delay
+      const timeout = setTimeout(() => {
+        setHasAnimationShown(true);
+        setItem('ppcoaAnimation', 'true', 'local');
+      }, 6500); // 6.5 seconds
+
+      return () => clearTimeout(timeout);
+    } else {
+      setHasAnimationShown(true); // Already shown, don't wait
+    }
+  }, [getItem, setItem]);
+
+  if (hasAnimationShown) {
     return null;
   }
-
-  setTimeout(() => {
-    if (showAnimation) {
-      setShowAnimation(false);
-    }
-  }, 6550);
-
+  
   return (
     <section className='fixed top-0 right-0 left-0 h-dvh w-full z-50 flex items-center justify-center bg-white animate-[fadeOut_1.5s_6.5s]'>
       <article className='flex items-center justify-center'>
