754174d968e509d459c749b8b426d8cc58b5a0b5
vitor-martinss <vitormartinsweb@gmail.com>
2024-05-02 09:50:35 +0100

    feat: upd gifs

--- src/components/Gif/index.tsx
+++ src/components/Gif/index.tsx
@@ -1,22 +1,24 @@
 'use client';
 
+import { useScreenSize } from '@/hooks/useScreenSize';
 import { useSessionStorage } from '@uidotdev/usehooks';
 import Image from 'next/image';
 import React, { useEffect } from 'react';
 
 // eslint-disable-next-line no-unused-vars
-function Gif({setRemoveAnimationBg}: {setRemoveAnimationBg: (value: React.SetStateAction<boolean>) => void}) {
+function Gif({ setRemoveAnimationBg }: { setRemoveAnimationBg: (value: React.SetStateAction<boolean>) => void }) {
   const [hasAnimationShown, setHasAnimationShown] = useSessionStorage('ppcoaAnimation', false);
+  const { isMediumAndUp } = useScreenSize();
 
   useEffect(() => {
     if (!hasAnimationShown) {
       const timeout = setTimeout(() => {
         setHasAnimationShown(true);
-      }, 6655);
+      }, 8000);
 
       return () => clearTimeout(timeout);
     } else {
-      setRemoveAnimationBg(true)
+      setRemoveAnimationBg(true);
       setHasAnimationShown(true);
     }
   }, [hasAnimationShown, setHasAnimationShown, setRemoveAnimationBg]);
@@ -28,9 +30,9 @@ function Gif({setRemoveAnimationBg}: {setRemoveAnimationBg: (value: React.SetSta
 
   return (
     <Image
-      className="h-full w-full object-contain"
+      className="h-full w-full object-cover"
       alt="gif"
-      src="/ppcoa-intro-medium.gif"
+      src={isMediumAndUp ? '/ppcoa-intro-medium.gif' : '/ppcoa-intro-small.gif'}
       fill
       sizes="100vw"
       unoptimized
--- src/components/IntroAnimation/index.tsx
+++ src/components/IntroAnimation/index.tsx
@@ -5,7 +5,7 @@ import { Gif } from '../Gif';
 import { useEffect, useState } from 'react';
 
 function IntroAnimation() {
-  const [removeAnimationBg, setRemoveAnimationBg] = useState(false)
+  const [removeAnimationBg, setRemoveAnimationBg] = useState(false);
   const isClient = useIsClient();
 
   useEffect(() => {
@@ -14,14 +14,15 @@ function IntroAnimation() {
     }
   }, [isClient, removeAnimationBg]);
 
-
   if (removeAnimationBg) {
     return null;
   }
 
   return (
-    <section className="fixed top-0 right-0 left-0 h-dvh w-full z-50 flex items-center justify-center bg-white animate-[fadeOut_1.5s_6.5s]">
-      <article className="flex items-center justify-center">{isClient ? <Gif setRemoveAnimationBg={setRemoveAnimationBg} /> : null}</article>
+    <section className="fixed top-0 right-0 left-0 h-dvh w-full z-50 flex items-center justify-center bg-white animate-[fadeOut_1.5s_8s]">
+      <article className="flex items-center justify-center">
+        {isClient ? <Gif setRemoveAnimationBg={setRemoveAnimationBg} /> : null}
+      </article>
     </section>
   );
 }
