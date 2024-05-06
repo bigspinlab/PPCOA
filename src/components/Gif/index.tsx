3366db2b33b8a82626fecab16e820d69ff50ea13
vitor-martinss <vitormartinsweb@gmail.com>
2024-05-06 17:18:54 +0100

    feat: changed animation

--- src/components/Gif/index.tsx
+++ src/components/Gif/index.tsx
@@ -4,6 +4,7 @@ import { useScreenSize } from '@/hooks/useScreenSize';
 import { useSessionStorage } from '@uidotdev/usehooks';
 import Image from 'next/image';
 import React, { useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 // eslint-disable-next-line no-unused-vars
 function Gif({ setRemoveAnimationBg }: { setRemoveAnimationBg: (value: React.SetStateAction<boolean>) => void }) {
@@ -14,7 +15,7 @@ function Gif({ setRemoveAnimationBg }: { setRemoveAnimationBg: (value: React.Set
     if (!hasAnimationShown) {
       const timeout = setTimeout(() => {
         setHasAnimationShown(true);
-      }, 7000);
+      }, 7500);
 
       return () => clearTimeout(timeout);
     } else {
@@ -23,20 +24,31 @@ function Gif({ setRemoveAnimationBg }: { setRemoveAnimationBg: (value: React.Set
     }
   }, [hasAnimationShown, setHasAnimationShown, setRemoveAnimationBg]);
 
-  if (hasAnimationShown) {
-    () => setRemoveAnimationBg(true);
-  }
+  useEffect(() => {
+    if (hasAnimationShown) {
+      setRemoveAnimationBg(true);
+    }
+  }, [hasAnimationShown, setRemoveAnimationBg]);
 
   return (
-    <Image
-      className="h-full w-full object-cover"
-      alt="gif"
-      src={isMediumAndUp ? '/ppcoa-intro-medium.gif' : '/ppcoa-intro-small.gif'}
-      fill
-      sizes="100vw"
-      unoptimized
-      priority
-    />
+    <AnimatePresence>
+      <motion.article
+        initial={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1.5 }}
+        className="flex items-center justify-center relative h-full w-full"
+      >
+        <Image
+          className="h-full w-full object-cover"
+          alt="gif"
+          src={isMediumAndUp ? '/ppcoa-intro-medium.gif' : '/ppcoa-intro-small.gif'}
+          fill
+          sizes="100vw"
+          unoptimized
+          priority
+        />
+      </motion.article>
+    </AnimatePresence>
   );
 }
 
--- src/components/IntroAnimation/index.tsx
+++ src/components/IntroAnimation/index.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import {  useState } from 'react';
+import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import dynamic from 'next/dynamic';
 
 const Gif = dynamic(() => import('../Gif'), {
-  ssr: false,
+  ssr: false
 });
 
 function IntroAnimation() {
@@ -17,12 +17,10 @@ function IntroAnimation() {
         <motion.section
           initial={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 1.5 }}
+          transition={{ duration: 0.2 }}
           className="fixed top-0 right-0 left-0 h-dvh w-full z-50 flex items-center justify-center bg-white"
         >
-          <article className="flex items-center justify-center">
-            <Gif setRemoveAnimationBg={setRemoveAnimationBg} />
-          </article>
+          <Gif setRemoveAnimationBg={setRemoveAnimationBg} />
         </motion.section>
       ) : null}
     </AnimatePresence>
